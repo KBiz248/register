92625c34625b5702e7a2d0a134e820a8ce3213c5
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-07-06 23:23:12 -0400

    add more drink information objects

--- drink-info.js
+++ drink-info.js
@@ -1,3 +1,29 @@
-const latte = {name: 'Latte', price: [2.45, 3.15, 3.79]};
-const cinnamonDolceLatte = {name: "Cinnamon Dolce Latte", price: [3.45, 3.99, 4.12]};
-const cappuccino = {name: "Cappuccino", price: [2.50, 3.00, 3.50]};
+const latte = {
+    name: "Latte",
+    size: ["tall", "grande", "venti"],
+    price: [2.45, 3.15, 3.79]
+};
+
+const cinnamonDolceLatte = {
+    name: "Cinnamon Dolce Latte",
+    size: ["tall", "grande", "venti"],
+    price: [3.45, 3.99, 4.12]
+};
+
+const cappuccino = {
+    name: "Cappuccino",
+    size: ["tall", "grande", "venti"],
+    price: [2.50, 3.00, 3.50]
+};
+
+const flatWhite = {
+    name: "Flat White",
+    size: ["tall", "grande", "venti"],
+    price: [2.45, 3.45, 4.15]
+};
+
+const honeyAlmondmilkFlatWhite = {
+    name: "Honey Almondmilk Flat White",
+    size: ["tall", "grande", "venti"],
+    price: [3.15, 3.75, 4.20]
+};
