f951f5fe4648ca355f2c979bb71917149aacdade
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-07-06 22:38:23 -0400

    add seperate script for objects containing drink information

---
+++ drink-info.js
@@ -0,0 +1,3 @@
+const latte = {name: 'Latte', price: [2.45, 3.15, 3.79]};
+const cinnamonDolceLatte = {name: "Cinnamon Dolce Latte", price: [3.45, 3.99, 4.12]};
+const cappuccino = {name: "Cappuccino", price: [2.50, 3.00, 3.50]};
